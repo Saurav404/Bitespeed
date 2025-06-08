93169aecb61119837f0e5e53abef8084debdb099
Saurav404 <saurav.rwt2611@gmail.com>
2025-06-08 15:14:53 +0530

    Update: Configuration changes for deployment

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,15 @@
-import dotenv from "dotenv";
-import { Dialect } from "sequelize";
+import dotenv from 'dotenv';
+import { Dialect } from 'sequelize';
 
-dotenv.config({ path: ".env" });
+dotenv.config({ path: '.env' });
 
 export const dbConfig = {
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
-  dialect: process.env.DB_TYPE as Dialect,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER, 
-  password: process.env.DB_PASSWORD,
+  environment: process.env.ENVIRONMENT || 'development',
+  url: process.env.DATABASE_URL || '',
+  host: process.env.DB_HOST || '',
+  port: parseInt(process.env.DB_PORT || '', 10) || 5432,
+  dialect: (process.env.DB_TYPE as Dialect) || 'postgres',
+  database: process.env.DB_NAME || '',
+  username: process.env.DB_USER || '',
+  password: process.env.DB_PASSWORD || '',
 };
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,20 +1,31 @@
-import { Sequelize } from "sequelize";
-import { dbConfig } from "../config/config";
+import { Sequelize } from 'sequelize';
+import { dbConfig } from '../config/config';
 
-const sequelizeConnection = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  {
+let sequelizeConnection: Sequelize;
+
+if (dbConfig.environment === 'production') {
+  sequelizeConnection = new Sequelize(dbConfig.url, {
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else {
+  sequelizeConnection = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
     host: dbConfig.host,
     port: dbConfig.port,
     dialect: dbConfig.dialect,
-  }
-);
+  });
+}
 
 const dbSync = async () => {
   try {
-    await sequelizeConnection.sync({ alter: true });
+    await sequelizeConnection.authenticate();
+    await sequelizeConnection.sync({ alter: true }); // Be careful with alter in production
     return { success: true };
   } catch (error) {
     throw error;
@@ -23,12 +34,12 @@ const dbSync = async () => {
 
 dbSync()
   .then(res => {
-   console.log('DB synced successfully!!!');
+    console.log('DB synced successfully!!!');
   })
   .catch(err => {
-    console.log("Failed to sync DB", err);
+    console.log('Failed to sync DB', err);
   });
 
 export { dbSync };
- 
+
 export default sequelizeConnection;
